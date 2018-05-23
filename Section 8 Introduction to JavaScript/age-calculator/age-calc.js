63a1b7f2f8c83766a9d3f26eb30fe3677cc2d5eb
Al <afsm1995@live.com>
2018-05-23 10:38:27 -0600

    Section complete

---
+++ Section 8 Introduction to JavaScript/age-calculator/age-calc.js
@@ -0,0 +1,3 @@
+var yearAge = prompt("How old are you in years?");
+var dayAge = yearAge * 365;
+window.alert("you are " + dayAge + " days old");
---
+++ Section 8 Introduction to JavaScript/including-scripts/including-scripts.js
@@ -0,0 +1,5 @@
+//window.alert("hello from the js file");
+
+var username = prompt("What is your name?");
+window.alert("Nice to meet you " + username);
+console.log("Also nice to meet you " + username);
